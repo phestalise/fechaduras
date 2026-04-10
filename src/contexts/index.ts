fee5821a82277afc8a00aaa3c6e68c9ae2293747
phestalise <phestalise@gmail.com>
2026-04-09 21:16:34 -0300

    limpando index

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,5 +1,3 @@
 
 export { AuthProvider } from './AuthContext';
 export { useAuth } from '../hooks/useAuth';
-export { CartProvider, useCart } from './CartContext';
-export type { Produto, ItemCarrinho } from './CartContext';
